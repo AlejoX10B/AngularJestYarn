3e583e54dc18393ca4f405211d33b41e251ed103
Alejo Castañeda <fabio.castaneda.michelada@kueski.com>
2023-10-01 22:52:09 -0500

    Added PrimeNG, PrimeIcons and PrimeFlex
    WIP

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PrimeNGConfig } from 'primeng/api';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.sass']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Domus';
+
+  constructor(private primengConfig: PrimeNGConfig) {
+  }
+
+  ngOnInit() {
+    this.primengConfig.ripple = true;
+  }
+
 }
